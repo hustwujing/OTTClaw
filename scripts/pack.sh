38e756fb72747b3617802d908ba491205387f682
hustwujing <wujing03@bilibili.com>
2026-03-21 01:03:03 +0800

    Update author info in scripts

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Author:    维杰（邬晶）
-# Email:     wujing03@bilibili.com
+# Author:    Vijay
+# Email:     hustwujing@163.com
 # Date:      2026
 # Copyright: Copyright (c) 2026 维杰（邬晶）
 #
